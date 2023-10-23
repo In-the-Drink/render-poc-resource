b6bd1557279430416b0e3c1d14a5603c56533894
Tim Davidson <tndavidson@gmail.com>
2023-10-23 01:42:39 -0600

    initial commit

---
+++ src/main/java/com/github/inthedrink/render/poc/resource/RenderPocResourceApplication.java
@@ -0,0 +1,13 @@
+package com.github.inthedrink.render.poc.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderPocResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderPocResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/inthedrink/render/poc/resource/RenderPocResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.inthedrink.render.poc.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderPocResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
